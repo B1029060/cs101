f8ae854879f4c73e5a37ec016455c176c1695cea
B1029060 <B1029060@cgu.edu.tw>
2022-03-28 21:02:02 +0800

    asddasasdd

---
+++ hw3/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define HEIGHT 16
+
+void hanoi(int n, char A, char B, char C) {
+    if(n == 1) {
+        printf("Move sheet from %c to %c\n", A, C);
+    }
+    else {
+        hanoi(n-1, A, C, B);
+        hanoi(1, A, B, C);
+        hanoi(n-1, B, A, C);
+    }
+}
+int main() {
+    clock_t begin, end;
+    double cost;
+    begin = clock();
+    hanoi(HEIGHT, 'A', 'B', 'C');
+    end = clock();
+    cost = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Time cost: %d\n", cost);
+    return 0;
+}
