19f6508936d1bed4c6ab6552b4f0135d681b4fb9
B1029060 <B1029060@cgu.edu.tw>
2022-03-31 16:58:56 +0800

    rtn

---
+++ lab3/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int my_strlen(char* s) {
+    int a = 0;
+    while (*s++ != NULL) a++;
+    return a;
+}    
+int rec_strlen(char* s) { int a = 0;
+    while (*s++ != NULL) {rec_strlen(s); a++;}
+    return a;
+} 
+
+// char* my_sort(char* s) {
+    
+
+// }
+int main () {
+    char s[] = "IU is a girl!";
+    printf("len = %d\n", my_strlen(s));
+    printf("len = %d\n", rec_strlen(s));
+    // printf("sort = %s\n", mysort(s));
+    return 0;
+}
