36853c4492220dc86427a8a36a28a9f4c7216b1c
B1029060 <B1029060@cgu.edu.tw>
2022-04-07 17:29:07 +0800

    gogogooogoogogo

---
+++ qz3/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+//void rec_dec (char* s) {
+//    if (!*s) return 0;   
+//    return s;
+//}
+void hanoi_tower(int n, char A, char B, char C) {
+    FILE* fp = fopen("hanoi.txt", "a+");
+    if(n == 1) {
+        fprintf("Move disk %d from %c to %c\n", n, A, C);
+    }
+    else {
+        hanoi(n-1, A, C, B);
+        hanoi(1, A, B, C);
+        hanoi(n-1, B, A, C);
+    }
+    fclose(fp);
+}
+int multiplication(int i, int j) {
+    if (j == 10) {
+        printf("\n");
+        j = 1;
+        i++;
+    }
+    printf("%dx%d = %d\t", i, j, i*j);
+    if (i == 9 && j == 9) return 0;
+    multiplication(i, ++j);
+}
+
+int main() {
+    char s[] = "1234567890";
+    //rec_dec(s);
+    //printf("func#1---\n");
+    hanoi_tower(16, 'A', 'B', 'C');
+    printf("func#2---\n");
+    multiplication(1, 1);
+    printf("func#3---\n");
+    return 0;
+}
