5a3b5eb1e9fdf527d1d0658b3df84004da00ce67
B1029060 <B1029060@cgu.edu.tw>
2022-04-07 22:58:43 +0800

    222

--- qz3/main.c
+++ qz3/main.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 
-// void rec_dec (char* s) {
-    // if (*s != NULL) printf("%c,", *s);
-    // if (*s != NULL) rec_dec(++s); 
-    // if (*s == NULL) printf("\n");
-// }
-void hanoi_tower(int n, char A, char B, char C) {
+void rec_dec (char* s) {
+    if (*s != NULL) printf("%c,", *s);
+    if (*s != NULL) rec_dec(++s); 
+    if (*s == NULL) printf("\n");
+}
+void hanoi(int n, char A, char B, char C) {
     FILE* fp = fopen("hanoi.txt", "w");
     if(n == 1) {
-        fprintf(fp, "Move disk %d from %c to %c\n", n, A, C);
+        fprintf(fp, "Move disk 1 from %c to %c\n", A, C);
     }
     else {
-        hanoi_tower(n-1, A, C, B);
-        hanoi_tower(1, A, B, C);
-        hanoi_tower(n-1, B, A, C);
+        hanoi(n-1, A, C, B);
+        hanoi(1, A, B, C);
+        hanoi(n-1, B, A, C);
     }
     fclose(fp);
 }
+
 int multiplication(int i, int j) {
     if (j == 10) {
         printf("\n");
@@ -30,9 +31,9 @@ int multiplication(int i, int j) {
 
 int main() {
     char s[] = "1234567890";
-    // rec_dec(s);
+    rec_dec(s);
     printf("func#1---\n");
-    hanoi_tower(16, 'A', 'B', 'C');
+    hanoi(16, 'A', 'B', 'C');
     printf("func#2---\n");
     multiplication(1, 1);
     printf("func#3---\n");
