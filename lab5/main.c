5a709e810b0a15177c177000cd0ac34ac65500da
B1029060 <B1029060@cgu.edu.tw>
2022-04-26 15:16:30 +0800

    44

--- lab5/main.c
+++ lab5/main.c
@@ -31,12 +31,14 @@ int main() {
     for (int i = 0; i < n; i++) set_value(ip1+i, i+1);
     
     print_array(ip1, n);
+    free(ip1);
     printf("No.2 ------------\n");
     array_list_t nn;
     nn.var = 20;
     int* ip2 = nn.Get_Int_Array(nn.var);
     for (int i = 0; i < nn.var; i++) nn.Set_Value(ip2+i, i+1);
     nn.Print_Array(ip2, nn.var);
+    free(ip2);
     printf("No.3 ------------");
     return 0;
 }
