5831fece9cb68dbca95c6538c915f80da6c76710
B1029060 <B1029060@cgu.edu.tw>
2022-04-28 18:53:55 +0800

    1

--- lab3/main.c
+++ lab3/main.c
@@ -9,8 +9,7 @@ int rec_strlen(char* s) {
     if (!*s) return 0;
     return 1 + rec_strlen(s+1);
 } 
-
-char* my_sort(char* s) {
+/* char* my_sort(char* s) {
     for (char* t = s; *t; t++) {
         for (char* p = t+1; *p; p++) {
             if (*t > *p) {
@@ -21,11 +20,11 @@ char* my_sort(char* s) {
         }
     }
     return s;
-}
+} */
 int main () {
     char s[] = "IU is a girl!";
     printf("len = %d\n", my_strlen(s));
     printf("len = %d\n", rec_strlen(s));
-    printf("sort = %s\n", mysort(s));
+    /*printf("sort = %s\n", mysort(s)); */
     return 0;
 }
