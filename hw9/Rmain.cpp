546d2cdfc20130f5dbaa571a1c431455a9d36561
B1029060 <91185329+B1029060@users.noreply.github.com>
2022-06-13 23:20:36 +0800

    Create Rmain.cpp

---
+++ hw9/Rmain.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+class ReplaceMyString {
+    private:
+        string str;
+    public:
+        ReplaceMyString() {
+            str = "IU";
+        }
+        replaceString(string origin, string alter) {
+            cout << "Old one : " << str ;
+            if (origin == str) str = alter;
+            cout << " -> New one : " << str << endl;
+        }
+};
+
+int main() {
+    ReplaceMyString my;
+    my.replaceString("IU", "IU is best");
+    return 0;
+}
